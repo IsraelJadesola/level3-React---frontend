aea339f739c9e437e5220c77b16d356b0e5a54c9
Atunwa Israel Jadesola <israeljadesola20000@gmail.com>
2025-11-24 17:17:20 +0100

    feat: Implement logout functionality in Dashboard component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Btn from '../components/Btn'
+import { useNavigate } from 'react-router-dom'
 
 const Dashboard = () => {
+  const navigate =  useNavigate()
+
 
   const logout = ((e) => {
     localStorage.removeItem("token")
     localStorage.removeItem("user")
+    navigate("/signin")
   })
 
   return (
